e61a599fa6207ebfd8473bfe71abd4fc1fa0a14d
Stephen-Hallett <stevohallett@gmail.com>
2025-10-14 18:31:43 +1300

    feat: Add manual portfolio update endpoint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,11 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from .config import settings
 from .router.ASB import router as ASBRouter
+from .router.ASB import save_data as save_asb
 from .router.BNZ import router as BNZRouter
+from .router.BNZ import save_data as save_bnz
 from .router.investnow import router as InvestnowRouter
 from .router.kernel_wealth import router as KernelRouter
+from .router.kernel_wealth import save_data as save_kernel
 from .router.sharesies import router as SharesiesRouter
+from .router.sharesies import save_data as save_sharesies
 from .router.simplicity import router as SimplicityRouter
+from .router.simplicity import save_data as save_simplicity
 from .utils.db import SavingsDB
 from .utils.logger import MyLogger
 
@@ -35,6 +40,16 @@ app.include_router(InvestnowRouter, prefix="/investnow")
 def portfolio_value() -> dict[str, dict | float]:
     print("Getting portfolio value")
     return db_con.current_portfolio()
+
+
+@app.post("/portfolio")
+def save_portfolio() -> str:
+    save_asb()
+    save_bnz()
+    save_kernel()
+    save_sharesies()
+    save_simplicity()
+    return "Portfolio Updated"
 
 
 @app.get("/health")
